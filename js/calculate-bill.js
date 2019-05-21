850764235f7af2004412b2bc7a4524d3d20d7904
victor-mthemba <victormthemba@gmail.com>
2019-05-21 13:44:27 +0200

    calculate-bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,9 +1,37 @@
 //get a reference to the calculate button
+var calculateBtn = document.querySelector(".calculateBtn");
 
 //get a reference to the billTotal element
+var billTotalElement = document.querySelector(".billTotal");
 
 //get a reference to the billString
+var billStringField = document.querySelector(".billString");
 
+
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringField.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+
+calculateBtn.addEventListener('click', calculateBtnClicked);
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
